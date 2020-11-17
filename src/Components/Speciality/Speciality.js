a37ae502e442eb479971d7fa472b864fc3fb86a8
KBA Sayem Aslam <67522021+KBA-Aslam@users.noreply.github.com>
2020-11-17 21:59:46 +0600

    first upload

---
+++ src/Components/Speciality/Speciality.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import sp1 from '../../image/sp-1.png';
+import sp2 from '../../image/sp-2.png';
+import sp3 from '../../image/sp-3.png';
+import './Speciality.css'
+
+function Speciality() {
+    return (
+        <div className="container mb-3">
+            <h3 className="mb-5"><span style={{color:'#FFC300'}}>WHY</span> CHOOSE US</h3>
+            <div className="card-deck">
+            <div className="card card-div  d-flex align-items-center">
+                <img src={sp1} className="card-img-top pt-3" alt="..."/>
+                <div className="card-body">
+                <h6 className="card-title font-weight-bold">FREE FITNESS TRAINING</h6>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+            <div className="card card-div d-flex align-items-center shadow bg-white rounded" style={{border: '1px solid #FFC300'}}>
+                <img src={sp2} className="card-img-top pt-3" alt="..."/>
+                <div className="card-body">
+                <h6 className="card-title font-weight-bold">TONS OF CARDIO & STREINGTH</h6>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+            <div className="card card-div  d-flex align-items-center">
+                <img  src={sp3} className="card-img-top pt-3" alt="..."/>
+                <div className="card-body">
+                <h6 className="card-title font-weight-bold">NO COMMITMENT MEMBERSHIPS</h6>
+                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Speciality;
---
+++ src/Components/Training/Training.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import train1 from '../../image/train1.jpg';
+import train2 from '../../image/train2.png';
+import './Training.css'
+
+function Training() {
+    return (
+        <div className="container mb-3">
+            <h3 className="mb-5">TRAINING <span style={{color:'#FFC300'}}>PROGRAMS</span></h3>
+            <div class="row row-cols-1 row-cols-md-2">
+  <div class="col mb-4">
+    <div class="card train-card">
+      <img src={train2} class="card-img-top" alt="..."/>
+      <div class="card-img-overlay font-weight-bold" style={{paddingTop:"50%", paddingRight:"50%"}}>
+        <p style={{backgroundColor:"#FFC300", height:"50px", marginLeft:"-20px", paddingTop:"10px"}}>YOGA TRAINING SESSION</p>
+  </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card train-card">
+      <img src={train1} class="card-img-top" alt="..."/>
+      <div class="card-img-overlay font-weight-bold" style={{paddingTop:"50%", paddingRight:"50%"}}>
+        <p style={{backgroundColor:"#FFC300", height:"50px", marginLeft:"-20px", paddingTop:"10px"}}>CARDIO TRAINING SESSION</p>
+  </div>
+    </div>
+  </div>
+        </div>
+        </div>
+    );
+};
+
+export default Training;
